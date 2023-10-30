7d77f7999e66f170a1014d1df45c12509150f007
hsynzge <149234647+hsynzge@users.noreply.github.com>
2023-10-30 20:56:50 +0300

    Initial commit

---
+++ Python ders/degiskenler.py
@@ -0,0 +1,22 @@
+baslik = ("HABERİNİZ OLSUN")
+vade = 12 #integer
+faizOrani = 1.47 #float
+faizOrani2 = 1.44
+faizOrani2 = 1.45
+
+print(baslik)
+print(type(baslik))
+print(type(vade))
+print(type(faizOrani))
+
+mesaj = "HOŞGELDİN"
+musteriAdi = "Hüseyin"
+musteriSoyadi = "Özge"
+sonucMesaj = (mesaj + " " + musteriAdi + " " + musteriSoyadi + "!" )
+print(sonucMesaj)
+
+sayi1 = 10
+sayi2 = 20
+print(sayi1+sayi2)
+
+print(sonucMesaj)
